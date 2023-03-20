5aab1bab1286fe8471b967310e076265f3dc1f6c
Dương Văn Sỹ <91702958+dvsteam@users.noreply.github.com>
2023-03-21 04:32:09 +0700

    Update config.bak.php

--- config.bak.php
+++ config.bak.php
@@ -6,9 +6,9 @@ $Sys_config["db_user"] = "root";
 $Sys_config["db_password"] = "123456";
 $Sys_config["db_database"] = "appleid_auto";
 
-$Sys_config["apiurl"] = "http://xxx.xxx"; // 站点地址，无需斜杠结尾
-$Sys_config["apikey"] = "114514"; // API密钥
-$Sys_config["webdriver_url"] = "http://"; // webdriver地址，需要带端口
+$Sys_config["apiurl"] = "http://auto.dbsteam.net"; // 站点地址，无需斜杠结尾
+$Sys_config["apikey"] = "dvsteam1122"; // API密钥
+$Sys_config["webdriver_url"] = "http://auto.dvsteam.net:4444"; // webdriver地址，需要带端口
 $Sys_config["enable_proxy_pool"] = false; // 是否启用代理池
 $Sys_config["proxy_auto_disable"] = false; // 当后端报告代理不可用时，是否自动禁用该代理
 $Sys_config["task_headless"] = false; // 是否启用任务后台运行，即不显示浏览器窗口
