8c18c1624f46999362a8ad228ca0a337ecb57504
Dương Văn Sỹ <91702958+dvsteam@users.noreply.github.com>
2023-03-21 04:46:54 +0700

    Update config.bak.php

--- config.bak.php
+++ config.bak.php
@@ -6,14 +6,14 @@ $Sys_config["db_user"] = "root";
 $Sys_config["db_password"] = "123456";
 $Sys_config["db_database"] = "appleid_auto";
 
-$Sys_config["apiurl"] = "http://auto.dbsteam.net"; // 站点地址，无需斜杠结尾
-$Sys_config["apikey"] = "dvsteam1122"; // API密钥
-$Sys_config["webdriver_url"] = "http://auto.dvsteam.net:4444"; // webdriver地址，需要带端口
-$Sys_config["enable_proxy_pool"] = false; // 是否启用代理池
-$Sys_config["proxy_auto_disable"] = false; // 当后端报告代理不可用时，是否自动禁用该代理
-$Sys_config["task_headless"] = false; // 是否启用任务后台运行，即不显示浏览器窗口
-$Sys_config["proxy_list"] = ["http", "socks5", "http+url", "socks5+url"]; // 代理类型列表
+$Sys_config["apiurl"] = "http://auto.dbsteam.net"; // địa chỉ trang web, không cần dấu gạch chéo
+$Sys_config["apikey"] = "dvsteam1122"; // Khóa API
+$Sys_config["webdriver_url"] = "http://auto.dvsteam.net:4444"; // địa chỉ webdriver, cần có cổng
+$Sys_config["enable_proxy_pool"] = false; // Có bật nhóm proxy hay không
+$Sys_config["proxy_auto_disable"] = false; // Có tự động tắt proxy khi phụ trợ báo cáo rằng proxy không khả dụng
+$Sys_config["task_headless"] = false; // Có cho phép tác vụ chạy ngầm hay không, nghĩa là không hiển thị cửa sổ trình duyệt
+$Sys_config["proxy_list"] = ["http", "socks5", "http+url", "socks5+url"]; // danh sách các loại proxy
 
-// 是否启用Telegram Bot. 用于通知账号解锁情况. 留空则不启用
+// Có bật Telegram Bot hay không. Nó được sử dụng để thông báo trạng thái mở khóa tài khoản. Để trống để tắt
 $Sys_config["telegram_bot_token"] = "";
 $Sys_config["telegram_bot_chatid"] = "";
